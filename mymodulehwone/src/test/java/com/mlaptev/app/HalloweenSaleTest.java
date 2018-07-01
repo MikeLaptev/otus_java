d87ff02f844ccc3cafeac028f21ef739f4d00020
Mikhail Laptev <mikhail.laptev@gmail.com>
2018-07-01 13:55:59 +0100

    Hwone (#2)
    Adding an additional step with implementation and step.

--- mymodulehwone/src/test/java/com/mlaptev/app/HalloweenSaleTest.java
+++ mymodulehwone/src/test/java/com/mlaptev/app/HalloweenSaleTest.java
@@ -1,4 +1,24 @@
 package com.mlaptev.app;
 
-public class HalloweenSaleTest {
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class HalloweenSaleTest {
+
+  @ParameterizedTest
+  @MethodSource(value = "hackerrankScenarios")
+  void hackerrankBasicTests(int p, int d, int m, int s, int expectedResult) {
+    assertThat(HalloweenSale.getMaximumNumberOfGamesToBuy(p, d, m, s)).isEqualTo(expectedResult);
+  }
+
+  private static Stream<Arguments> hackerrankScenarios() {
+    return Stream.of(
+        Arguments.of((Object[]) new Integer[]{20, 3, 6, 80, 6}),
+        Arguments.of((Object[]) new Integer[]{20, 3, 6, 85, 7})
+    );
+  }
 }
