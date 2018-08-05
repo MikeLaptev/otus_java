3a42b320c30e0c2f11e6ca69287f61bfceccbdce
Mikhail Laptev <mikhail.laptev@gmail.com>
2018-08-05 16:31:22 +0100

    Adding templates for homeworks three to five

---
+++ mymodulehwfour/src/main/java/com/mlaptev/app/App.java
@@ -0,0 +1,13 @@
+package com.mlaptev.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
